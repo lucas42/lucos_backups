f01a11906bda805c3211debea26ab1c438a3f278
Luke Blaney <git@lukeblaney.co.uk>
2025-11-17 11:21:43 +0000

    only start ssh-agent if a new instance is needed, to avoid agent proccesses gradually acumulating over time

--- src/scripts/init-agent.sh
+++ src/scripts/init-agent.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-eval `ssh-agent -s`
+[ -n "$SSH_AUTH_SOCK" ] || eval `ssh-agent -s`
 echo "$SSH_PRIVATE_KEY" | sed 's/~/=/g' | ssh-add - # Padding characters are stored as tildas due to limitation in lucos_creds
